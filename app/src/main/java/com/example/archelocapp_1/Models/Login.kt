ca2f81b7430f264af5d3785b137933b7d58909a5
shasan06 <shasan06@dcs.bbk.ac.uk>
2021-08-13 00:26:11 +0100

    Login class and Login API is implemented

--- app/src/main/java/com/example/archelocapp_1/Models/Login.kt
+++ app/src/main/java/com/example/archelocapp_1/Models/Login.kt
@@ -1,5 +1,12 @@
 package com.example.archelocapp_1.Models
 
+import com.squareup.moshi.Json
+
+//Login is the model class that takes user credential in the form of json data
 data class Login(
-    var key : String
+    @field:Json(name = "key") var key: String
 )
+
+
+
+
---
+++ app/src/main/java/com/example/archelocapp_1/Models/LoginAPI.kt
@@ -0,0 +1,53 @@
+package com.example.archelocapp_1.Models
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.coroutines.Deferred
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.*
+import java.lang.Boolean.FALSE
+
+// Support functions and class to setup Login API request
+private val BASE_URL =
+    "http://archaelon.roussos.mobi/"
+
+private val logging = HttpLoggingInterceptor()
+    .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+private val httpClient = OkHttpClient.Builder()
+    .addInterceptor(logging)
+    .followRedirects(FALSE)
+    .followSslRedirects(FALSE)
+    .build();
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(BASE_URL)
+    .client(httpClient)
+    .build()
+
+
+// Define API end points
+interface LoginApiService {
+    //to submit the authenticated user credentials through post method to curl location
+    //request post to http://archaelon.roussos.mobi/login/
+    @POST("login/")
+    fun authenticateUser(@Body body: MultipartBody):
+            Deferred<Login>
+
+}
+
+object LoginApi {
+    val retrofitService : LoginApiService by lazy {
+        retrofit.create(LoginApiService::class.java) }
+}
